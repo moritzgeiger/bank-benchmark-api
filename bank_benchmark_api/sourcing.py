52a1c2d868978b7e81b08521d069faca7a6beec3
Moritz Geiger <moritzgeiger@hotmail.de>
2021-03-31 18:26:45 +0200

    pushed most code to modules

--- bank_benchmark_api/sourcing.py
+++ bank_benchmark_api/sourcing.py
@@ -13,14 +13,14 @@ import cloudinary.uploader
 import json
 
 # load banks file
-with open('../raw_data/banks.json') as json_file:
+with open('raw_data/banks.json') as json_file:
     banks = json.load(json_file)
 
 class PdfSourcing:
     def __init__(self, banks=banks):
         self.banks = banks
 
-    def find_price_pages(banks=banks, search=['preçário', 'pricelist', 'precario']):    
+    def find_price_pages(self, banks=banks, search=['preçário', 'pricelist', 'precario']):    
         search = [x.lower() for x in search]
         for k, v in banks.items():
             url = v.get('url')
@@ -45,48 +45,48 @@ class PdfSourcing:
                 print(f'could not reach page: {url}')
         
         ## SAVE BANK.json back to directory
-        with open('../raw_data/banks.json', 'w') as fp:
+        with open('raw_data/banks.json', 'w') as fp:
             json.dump(banks, fp)
         
         return banks
 
-    def get_pdf_urls(banks=banks):
-    for val in banks.values():
-        url = val.get('pricelist_url')
-        val['pdfs'] = list()
-        # some banks direclty link to a pdf address
-        if '.pdf' in url:
-            print(f'url is already pdf for: {url}')
-            val['pdfs'].append(f'{url}')
-        # for other landing pages look for every pdf on page
-        else:
-            r = requests.get(url)
-#             print(type(val['pdfs']))
-            if r.status_code == 200:
-                soup = BeautifulSoup(r.content, 'html.parser')
-                print(f'looking for pdfs in: {url}')
-                for link in soup.find_all('a', href=True):
-                    if '.pdf' in link.get('href'):
-                        # some pdf links are absolute links
-                        ## tru out r.url
-                        pdf = urljoin(val.get('url'),link.get('href'))
-                        print(f'found and added pdf: {pdf}')
-                        val['pdfs'].append(f'{pdf}')
+    def get_pdf_urls(self, banks=banks):
+        for val in banks.values():
+            url = val.get('pricelist_url')
+            val['pdfs'] = list()
+            # some banks direclty link to a pdf address
+            if '.pdf' in url:
+                print(f'url is already pdf for: {url}')
+                val['pdfs'].append(f'{url}')
+            # for other landing pages look for every pdf on page
             else:
-                print(f'could not execute parsing for: {url}')
+                r = requests.get(url)
+    #             print(type(val['pdfs']))
+                if r.status_code == 200:
+                    soup = BeautifulSoup(r.content, 'html.parser')
+                    print(f'looking for pdfs in: {url}')
+                    for link in soup.find_all('a', href=True):
+                        if '.pdf' in link.get('href'):
+                            # some pdf links are absolute links
+                            ## tru out r.url
+                            pdf = urljoin(val.get('url'),link.get('href'))
+                            print(f'found and added pdf: {pdf}')
+                            val['pdfs'].append(f'{pdf}')
+                else:
+                    print(f'could not execute parsing for: {url}')
+            
+            ## SAVE BANK.json back to directory
+            with open('raw_data/banks.json', 'w') as fp:
+                json.dump(banks, fp)
+
+            return banks
+
+    def rerun_sourcing(self, banks=banks):
+        banks = self.find_price_pages()
+        banks = self.get_pdf_urls(banks)
         
         ## SAVE BANK.json back to directory
-        with open('../raw_data/banks.json', 'w') as fp:
-            json.dump(banks, fp)
-
-        return banks
-
-    def rerun_sourcing(banks=banks):
-        banks = find_price_pages()
-        banks = get_pdf_urls()
-        
-        ## SAVE BANK.json back to directory
-        with open('../raw_data/banks.json', 'w') as fp:
+        with open('raw_data/banks.json', 'w') as fp:
             json.dump(banks, fp)
 
         return banks
