76b3b98be4b6c1121bd7de695e52bf0a821c86fd
Moritz Geiger <moritzgeiger@hotmail.de>
2021-04-05 14:32:07 +0200

    add background job for sourcing

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import flask
 from flask import request, jsonify
 import requests
+import os
+import time
+import json
+from threading import Thread
 
 
 ### importing classes
 from bank_benchmark_api.add_bank import AddBank
 # from bank_benchmark_api.sourcing import PdfSourcing
-from bank_benchmark_api.sourcing_each import PdfSourcing
+from bank_benchmark_api.sourcing import PdfSourcing
 
 ## init app
 app = flask.Flask(__name__)
@@ -23,7 +27,9 @@ app.config["DEBUG"] = True
 def home():
     return '''<h1>Welcome to the benchmark API</h1>
                 <p>A prototype API to get banking prices.</p>
-                <p>endpoint: /getpdfs (post) accepts dictionary of dictionaries</p>
+                <p>endpoint: /dopdfs (post) accepts dictionary of dictionaries</p>
+                <p>endpoint: /retrievepdfs (returns the newly created banks file)</p>
+                
                 
                 <p>endpoint: /meme (returns a meme)</p>
                 <p>endpoint: /ping (returns "pong")</p>
@@ -58,35 +64,14 @@ def reverse():
 
     return word[::-1]
 
-############################
-######## STATUS UPDATES ####
-############################
-
-# not done yet
-@app.route('/bankstatus', methods=['GET'])
-def bank_status():
-    if 'url' in request.args:
-        url = request.args['url']
-        headers = {'Accept-Language': 'pt-PT'}
-        response = requests.get(url, headers=headers)
-        return {'status':response.status_code, 'url':url}
-    else:
-        return {'error': {'message':'no bank url provided'}}
-
-@app.route('/pdfstatus', methods=['GET'])
-def pdf_status():
-    pass ## return pdf size or num of pdfs
-    ## todo: what endpoints do we need
-    # todo: route to check for new data
-
 
 #########################
 ##### BANK DETAILS ######
 #########################
 
-@app.route('/getpdfs', methods=['POST'])
-def get_pdfs():
-    print('get_pdfs was called')
+@app.route('/dopdfs', methods=['POST'])
+def do_pdfs():
+    print('fetch_pdfs was called')
     requirements = ['url', 'name', 'num_pdfs', 'last_updated', 'sum_sizes', 'bp_bank_id']
     validator = []
     r = request.json
@@ -96,15 +81,34 @@ def get_pdfs():
     validator = [j for i in validator for j in i]
     print(f'matched these positions in keys: {validator}')
     
-    if all(validator):
-        print(f'sorcing has started')
-        sourcing = PdfSourcing(r)
-        banks = sourcing.rerun_sourcing()
-        return banks
-    
+    if all(validator):  
+        # moving sourcing tasks to the background so that rails app has a quick reaponse
+
+        def start_task(r):
+            # runs all the sourcing jobs and then dumps jsonfile to 'bank_benchmark_api/data/banks.json
+            sourcing = PdfSourcing(r)
+            sourcing.rerun_sourcing()
+
+        thread = Thread(target=start_task, kwargs={'r':r})
+        thread.start()
+        return {'message': 'sourcing initialized. Go to /retrievepdfs to pick up requested data'}
+
     else:
         return {'error': f'one of these required keys were not passed {requirements}'}
 
+@app.route('/retrievepdfs', methods=['GET'])
+def retrieve_pdfs():
+    print('retrieve_pdfs was called')
+    try:
+        time.sleep(5)
+        with open('bank_benchmark_api/data/banks.json') as json_file:
+            banks = json.load(json_file)
+        os.remove('bank_benchmark_api/data/banks.json')
+        print(f'bank json loaded, supplied and removed from server')
+        return banks
+
+    except Exception as e:
+        return f'{{error: sourcing job not finished or initialized. first call /dopdfs and wait for backgroundjob to finish. Error msg: {e}}}'
 
 ## run app
 if __name__ == '__main__':
