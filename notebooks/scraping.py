dc90f478f11753fe416c06bb2e1b2dbb5cd07c35
Olga Cravesana <olga.cravesana5@gmail.com>
2021-04-07 15:30:07 +0100

    new function implemented for dictionary of pedro

--- notebooks/scraping.py
+++ notebooks/scraping.py
@@ -5,34 +5,38 @@ import re
 import nltk
 
 
-commission = {'abanca': {'1':'Emissão de extrato','2':'Fotocópias de segundas vias de talões de depósito',\
-                        '3': 'Manutenção de conta', '4': 'Levantamento de numerário', '5': 'Adesão ao serviço de banca à distância', '6':'Depósito de moedas metálicas',\
-                        '7':'Alteração de titulares'}, \
-              'ctt':{'1':'Extrato Integrado', '2':'Emissão 2ªs Vias de Avisos e Outros Documentos',\
-                     '3':'Comissão de manutenção de conta','4':'Levantamento de Numerário ao Balcão',
-                     '5':'Comissões por descoberto bancário','6':'Consulta de Movimentos de conta DO com', '7': 'Consulta de Saldo de conta DO com comprovativo',\
-                     '8':'Alteração de titularidade'},
-              'bai':{'1':'Manutenção de conta','2':'Levantamento de numerário','3':'Levantamento usd em contas usd',\
-                    '4': 'Extracto integrado','5':'Extracto avulso'},
-             'bankinter':{'1':'Comissão de Manutenção de Conta', '2': 'Comissão de Alteração de Titularidade',
-                          '3':'Comissão de Levantamento'},
-             'bic': {'1':'Comissão de manutenção de conta','2':'Segundas vias (pedido na agência)','3':'Levantamento de numerário',
-                    '4': 'Pedido de saldo ao balcão', '5':'Descoberto bancário', '6':'Depósito de moeda metálica',
-                    '7':'Alteração de titularidade'}}
+com_dict = {'Emissão de Extrato':['Emissão de extrato', 'Extrato Integrado', 'Extrato Mensal'],
+           'Fotocópias e 2ªvias':['Fotocópias de segundas vias de talões de depósito',
+                                  'Emissão 2ªs Vias de Avisos e Outros Documentos', 'Extracto avulso',
+                                 'Segundas vias (pedido na agência)'],
+           'Manutenção de Conta':['Manutenção de conta', 'Comissão de manutenção de conta'],
+           'Levantamento de Numerário':['Levantamento de numerário', 'Levantamento de numerário ao balcão'],
+           'Adesão ao Serviço Online':['Adesão ao serviço de banca à distância', 'Adesão ao serviço online'],
+            'Depósitos de Moedas':['Depósito de moedas metálicas', 'Depósito de moedas',
+                                   'Depósito de moedas ao balcão', 'Depósito de dinheiro ao balcão',
+                                  'Depósito de moeda metálica (≥ a 100 moedas)'],
+            'Ateração de Titulares':['Alteração de titulares', 'Alteração de titularidade',
+                                     'Alteração de titularidade / intervenientes'],
+            'Descoberto Bancário':['Comissões por descoberto bancário', 'Descoberto bancário'],
+            'Consulta de Movimentos':['Consulta de Movimentos de conta DO com', 'Consulta de movimentos ao balcão'],
+            'Consulta de Saldo':['Pedido de saldo ao balcão', 'Consulta de Saldo de conta DO com comprovativo']
+           }
+
+
 
 
 
 
 class Scraping:
-    def __init__(self, pdf, page, x, splitting_right=False):
+    def __init__(self, pdf, page, x):
         self.pdf = pdf
-        self.page = page
+        self.page = [page]
         self.x = x
-        self.splitting_right = splitting_right
 
-    def extract_clean(self):
+
+    def getting_text(self):
         file = pdfplumber.open(self.pdf)
-        if len(self.page) > 1 :
+        if len([self.page]) > 1 :
             joined_text = []
             for el in self.page:
                 page = file.pages[el]
@@ -42,70 +46,65 @@ class Scraping:
             text = text.replace('Isento', '0,00')
             text = text.replace('n/a', str(np.nan))
             text = re.sub('--', str(np.nan), text)
-            if nltk.sent_tokenize(text):
-                if self.splitting_right == True:
-                    text = text.replace('\n','. ')
-                    # text = len_sentences(text)
-                    text = nltk.sent_tokenize(text)
-                    return text
-                text = text.replace('\n',' ')
-                # text = len_sentences(text)
-                text = nltk.sent_tokenize(text)
             return text
         else:
-            page = file.pages[self.page[0]]
+            page = file.pages[[self.page][0]]
             text = page.extract_text()
             text = text.replace('Isento', '0,00')
             text = text.replace('n/a', str(np.nan))
             text = re.sub('--', str(np.nan), text)
-            if nltk.sent_tokenize(text):
-                if self.splitting_right == True:
-                    text = text.replace('\n','. ')
-                    # text = len_sentences(text)
-                    text = nltk.sent_tokenize(text)
-                    return text
-                text = text.replace('\n',' ')
-                # text = len_sentences(text)
-                text = nltk.sent_tokenize(text)
             return text
 
-    def search_com(self):
-        decod = {}
-        if self.x not in commission:
-            return 'not in the dictionary'
-        decod = commission[self.x]
-        file = self.extract_clean()
-        return decod, file
+    def tokenize(self):
+        text = self.getting_text()
+        if len(nltk.sent_tokenize(text)) < 15:
+            text = text.replace('\n','. ')
+            # text = len_sentences(text)
+            text = nltk.sent_tokenize(text)
+            return text
+        text = text.replace('\n',' ')
+        # text = len_sentences(text)
+        text = nltk.sent_tokenize(text)
+        return text
+
+    # def search_com(self):
+    #     decod = {}
+    #     if self.x not in commission:
+    #         return 'not in the dictionary'
+    #     decod = commission[self.x]
+    #     file = self.tokenize()
+    #     return decod, file
 
     def values(self):
-        file = self.search_com()[1]
-        names = self.search_com()[0]
-        lista = {}
-        keys = [x for x in names.keys()]
-        for key in keys:
+        file = self.tokenize()
+        values = [x for x in com_dict.values()]
+        lista ={}
+        for commission in values:
+            for value in commission:
                 for ind,sentence in enumerate(file):
-                    if names[key] in sentence:
-                        if key in lista:
+                    if value in sentence:
+                        if value in lista:
                             if '[0-9]{1-2},[0-9]{2}' in sentence:
-                                lista[names[key]]= lista[key].append(sentence)
+                                lista[value]= lista[value].append(sentence)
                             else:
-                                lista[names[key]]= lista[key].append(' '.join([sentence,file[ind+1]]))
+                                lista[value]= lista[value].append(' '.join([sentence,file[ind+1]]))
                         else:
                             if '[0-9]{1-2},[0-9]{2}' in sentence:
-                                lista[names[key]]= [sentence]
+                                lista[value]= [sentence]
                             else:
-                                lista[names[key]]= [' '.join([sentence,file[ind+1]])]
+                                lista[value]= [' '.join([sentence,file[ind+1]])]
         return lista
 
     def n_account(self):
         accounts = []
         finals = []
-        if self.splitting_right == True:
-            for sentence in self.search_com()[1]:
+        text = self.getting_text()
+        if len(nltk.sent_tokenize(text)) < 15 :
+            for sentence in self.tokenize():
                 if 'Conta' in sentence:
                     finals.append(sentence)
             return finals
-        for sentence in self.search_com()[1]:
+        for sentence in self.tokenize():
             contas = re.split('Conta',sentence)
             accounts.append(contas)
         for account in accounts:
@@ -132,5 +131,3 @@ class Scraping:
             if start not in regular:
                 regular.append(final)
         return regular
-
-
