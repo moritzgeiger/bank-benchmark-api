80fe0484d3df14a739cd27b3126e7a873331eede
Moritz Geiger <moritzgeiger@hotmail.de>
2021-04-15 11:22:13 +0200

    add headless

--- bank_benchmark_api/sourcing.py
+++ bank_benchmark_api/sourcing.py
@@ -67,6 +67,7 @@ class PdfSourcing:
                     chrome_options = webdriver.ChromeOptions()
                     chrome_options.add_argument('--disable-gpu')
                     chrome_options.add_argument('--no-sandbox')
+                    chrome_options.add_argument("--headless")
                     chrome_options.binary_location = GOOGLE_CHROME_PATH
                     driver = webdriver.Chrome(
                         execution_path=CHROMEDRIVER_PATH,
@@ -184,6 +185,7 @@ class PdfSourcing:
                         chrome_options = webdriver.ChromeOptions()
                         chrome_options.add_argument('--disable-gpu')
                         chrome_options.add_argument('--no-sandbox')
+                        chrome_options.add_argument("--headless")
                         chrome_options.binary_location = GOOGLE_CHROME_PATH
                         driver = webdriver.Chrome(
                         execution_path=CHROMEDRIVER_PATH,
